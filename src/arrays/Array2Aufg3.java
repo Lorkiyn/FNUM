479444b3d1ade8692c0c0e9ac6af900c9e7cef3d
Lorkiyn <dari18@online.de>
2016-04-22 12:44:05 +0200

    Just Uploaded

---
+++ src/arrays/Array2Aufg3.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class Array2Aufg3 {
+
+	public static void main ( String[] args ) {
+		int[] sumArray = new int[4];
+
+		int[][] data = {
+
+				{3, 2, 5, 7},
+
+				{1, 4, 4, 8},
+
+				{9, 1, 0, 2},
+
+				{0, 2, 6, 3} };
+
+		// Summe berechnen
+
+		for ( int zeile=0; zeile < data.length; zeile++) {
+
+			for ( int spalte = 0; spalte < data[zeile].length; spalte++){
+				sumArray[spalte] += data[zeile][spalte];
+			}
+
+		}
+
+		// Summe ausgeben
+		for (int i = 0; i < sumArray.length;i++) System.out.println(i+1 +". Summe Spalte: " +sumArray[i]);
+
+
+	}
+
+}
