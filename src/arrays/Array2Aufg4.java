256f8584ae68259dd6b03edc7c066e0a9e27b175
Lorkiyn <dari18@online.de>
2016-04-22 12:51:29 +0200

    Just Uploaded

---
+++ src/arrays/Array2Aufg4.java
@@ -0,0 +1,37 @@
+package arrays;
+
+public class Array2Aufg4 {
+
+	public static void main ( String[] args ) {
+		int minima = 0;
+		int maxima = 0;
+
+		int[][] data = {
+
+				{3, 2, 5, 7},
+
+				{1, 4, 4, 8},
+
+				{9, 1, 0, 2},
+
+				{0, 2, 6, 3} };
+
+		// Summe berechnen
+
+		for ( int zeile=0; zeile < data.length; zeile++) {
+
+			for ( int spalte = 0; spalte < data[zeile].length; spalte++){
+				if (data[zeile][spalte] < minima) minima = data[zeile][spalte];
+				if (data[zeile][spalte] > maxima) maxima = data[zeile][spalte];
+			}
+
+		}
+
+		// Summe ausgeben
+		System.out.println("Minima: " +minima
+				+"\nMaxima: " +maxima);
+
+
+	}
+
+}
