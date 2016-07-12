46d90d6fba462cf070ef45df1003ab9b24127c85
Lorkiyn <dari18@online.de>
2016-07-12 23:20:25 +0200

    Changes

--- src/package2/HelloWorld.java
+++ src/package2/HelloWorld.java
@@ -2,8 +2,8 @@ package package2;
 
 public class HelloWorld
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
 	}
 }
